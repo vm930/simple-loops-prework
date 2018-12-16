0a30d81d395e8367cf872fda72801ee3250b6fe2
abstract-animation-9914 <vm930@hotmail.com>
2018-12-16 02:46:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,7 +17,7 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times
+  number_of_times.times do puts phrase
 end
 
 def while_iterator(number_of_times)
